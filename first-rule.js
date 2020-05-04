13804e1927f14573ecb121c35dc394402d286fde
cicomsa <ioanacomsa85@gmail.com>
2020-05-04 21:06:53 +0100

    new logic based on index of live cells

--- first-rule.js
+++ first-rule.js
@@ -1,40 +1,53 @@
 // Each live cell with one or no neighbors dies, as if by solitude.
 
-const checkArray = (arr1Length, arr2Length, i) => {
-  if (arr1Length < 2 && arr2Length <= 1) {
-    return ['', '', '']
+const liveCells = arr => {
+  const result = arr.reduce(function (a1, e, i) {
+    if (e === 'o')
+      a1.push(i);
+    return a1;
+  }, [])
+
+  return result
+}
+
+const getLiveCellsTruth = (arr, truth, mainIndex) => {
+  if (arr.length === 1) {
+    const index = arr[0]
+    truth.push(index === mainIndex || index === mainIndex - 1 || index === mainIndex + 2)
   }
-  return null
+}
+
+const checkLiveCellsTruth = (arr1, arr2, arr3, truth, index, initialState) => {
+  if (arr1.length === 1) {
+    const mainIndex = arr1[0]
+
+    getLiveCellsTruth(arr2, truth, mainIndex)
+    getLiveCellsTruth(arr3, truth, mainIndex)
+
+    if (truth.length < 2) {
+      initialState.splice(index, 1, ['', '', '']);
+    }
+  }
 }
 
 const firstRule = initialState => {
   let cloneInitialState = [...initialState];
 
-  initialState.map((arr, i) => {
-    const arrLength = arr => arr.filter(el => el === 'o').length
+  const arr1 = initialState[0]
+  const arr2 = initialState[1]
+  const arr3 = initialState[2]
 
-    if (i === 2) {
-      // console.log(
-      //   arrLength(initialState[1]) < 2
-      //   && (arrLength(initialState[0]) === 0 || arrLength(initialState[0]) < 2)
-      //   && (arrLength(initialState[2]) === 0 || arrLength(initialState[2]) < 2),
-      //   arrLength(initialState[1]) < 2, initialState[1],
-      //   arrLength(initialState[0]) === 0 || arrLength(initialState[0]) < 2, initialState[0],
-      //   arrLength(initialState[2]) === 0 || arrLength(initialState[2]) < 2, initialState[2]
-      // )
-      if (
-        arrLength(initialState[1]) < 2
-        && (arrLength(initialState[0]) === 0 || arrLength(initialState[0]) < 2)
-        && (arrLength(initialState[2]) === 0 || arrLength(initialState[2]) < 2)
-      ) {
-        cloneInitialState.splice(1, 1, ['', '', '']);
-      }
-    } else {
-      cloneInitialState.splice(0, 1, checkArray(arrLength(initialState[0]), arrLength(initialState[1]), 0));
+  let truth1 = []
+  let truth2 = []
+  let truth3 = []
 
-      cloneInitialState.splice(2, 1, checkArray(arrLength(initialState[2]), arrLength(initialState[1]), 2));
-    }
-  })
+  const a1 = liveCells(arr1)
+  const a2 = liveCells(arr2)
+  const a3 = liveCells(arr3)
+
+  checkLiveCellsTruth(a1, a2, a3, truth1, 0, cloneInitialState)
+  checkLiveCellsTruth(a2, a1, a3, truth2, 1, cloneInitialState)
+  checkLiveCellsTruth(a3, a1, a2, truth3, 2, cloneInitialState)
 
   return cloneInitialState
 }
