d999852cdf7ad6cb1b3d64dbf25688c1e47fd0d1
cicomsa <ioanacomsa85@gmail.com>
2020-05-07 13:53:07 +0100

    rename variables

--- first-rule.js
+++ first-rule.js
@@ -1,11 +1,11 @@
 // Each live cell with one or no neighbors dies, as if by solitude.
 const { version1, cells } = require('./helpers')
 
-const newArray = (condition, newState, index, changeArray = false) => {
+const newArray = (condition, newState, index, modifyArray = false) => {
   const array = [...version1]
 
   if (condition) {
-    if (changeArray) array[1] = 'o'
+    if (modifyArray) array[1] = 'o'
     newState.splice(index, 1, array)
   }
 }
@@ -14,13 +14,13 @@ const addElement = (condition, els, el) => {
   if (condition) els.push(el)
 }
 
-const getTruth = (getLiveCellsTruth, arr1, arr2, truth) => {
+const getTruth = (getCellsTruth, arr1, arr2, truth) => {
   if (arr2.length) {
-    getLiveCellsTruth1(arr1, arr2, truth)
+    getCellsTruth(arr1, arr2, truth)
   }
 }
 
-const getLiveCellsTruth1 = (arr1, arr2, truth) => {
+const getCellsTruth1 = (arr1, arr2, truth) => {
   arr1.forEach(mainIndex => {
     arr2.map(index =>
       addElement(
@@ -34,29 +34,29 @@ const getLiveCellsTruth1 = (arr1, arr2, truth) => {
   })
 }
 
-const getLiveCellsTruth2 = (arr1, arr2, truth) => {
+const getCellsTruth2 = (arr1, arr2, truth) => {
   if (!arr2.length) {
-    let els = []
+    const els = []
 
     arr1.map((el, i) => addElement(arr1[i + 1] === el + 1, els, el))
     addElement(els[0] === els[1] - 1, truth, true)
   }
 }
 
-const checkLengthOne = (arr1, arr2, arr3, truth, index, newState, initialState) => {
+const checkSingles = (arr1, arr2, arr3, truth, index, newState, initialState) => {
   if (arr1.length === 1) {
-    getTruth(getLiveCellsTruth1, arr1, arr2, truth)
-    getTruth(getLiveCellsTruth1, arr1, arr3, truth)
+    getTruth(getCellsTruth1, arr1, arr2, truth)
+    getTruth(getCellsTruth1, arr1, arr3, truth)
 
     newArray(truth.length < 2, newState, index)
   }
 }
 
-const checkLengthTwo = (arr1, arr2, arr3, truth, index, newState, initialState) => {
+const checkDoubles = (arr1, arr2, arr3, truth, index, newState, initialState) => {
   if (arr1.length === 2) {
     if (arr1[0] === arr1[1] - 1) {
-      getTruth(getLiveCellsTruth1, arr1, arr2, truth)
-      getTruth(getLiveCellsTruth1, arr1, arr3, truth)
+      getTruth(getCellsTruth1, arr1, arr2, truth)
+      getTruth(getCellsTruth1, arr1, arr3, truth)
     }
 
     newArray(truth.length === 0, newState, index)
@@ -69,10 +69,10 @@ const checkLengthTwo = (arr1, arr2, arr3, truth, index, newState, initialState) 
   }
 }
 
-const checkLengthThree = (arr1, arr2, arr3, truth, index, newState, initialState) => {
+const checkTriples = (arr1, arr2, arr3, truth, index, newState, initialState) => {
   if (arr1.length === 3) {
-    getLiveCellsTruth2(arr1, arr2, truth)
-    getTruth(getLiveCellsTruth2, arr1, arr3, truth)
+    getCellsTruth2(arr1, arr2, truth)
+    getTruth(getCellsTruth2, arr1, arr3, truth)
 
     newArray(
       (truth.length === 1 || truth.length === 2)
@@ -84,10 +84,10 @@ const checkLengthThree = (arr1, arr2, arr3, truth, index, newState, initialState
   }
 }
 
-const checkLiveCellsTruth = (arr1, arr2, truth, index, newState, initialState, arr3 = []) => {
-  checkLengthOne(arr1, arr2, arr3, truth, index, newState, initialState)
-  checkLengthTwo(arr1, arr2, arr3, truth, index, newState, initialState)
-  checkLengthThree(arr1, arr2, arr3, truth, index, newState, initialState)
+const checkCellsTruth = (arr1, arr2, truth, index, newState, initialState, arr3 = []) => {
+  checkSingles(arr1, arr2, arr3, truth, index, newState, initialState)
+  checkDoubles(arr1, arr2, arr3, truth, index, newState, initialState)
+  checkTriples(arr1, arr2, arr3, truth, index, newState, initialState)
 }
 
 const firstRule = initialState => {
@@ -101,9 +101,9 @@ const firstRule = initialState => {
 
   const { arr1, arr2, arr3, truth1, truth2, truth3 } = resultsObject
 
-  checkLiveCellsTruth(arr1, arr2, truth1, 0, newState, initialState)
-  checkLiveCellsTruth(arr2, arr1, truth2, 1, newState, initialState, arr3)
-  checkLiveCellsTruth(arr3, arr2, truth3, 2, newState, initialState)
+  checkCellsTruth(arr1, arr2, truth1, 0, newState, initialState)
+  checkCellsTruth(arr2, arr1, truth2, 1, newState, initialState, arr3)
+  checkCellsTruth(arr3, arr2, truth3, 2, newState, initialState)
 
   return newState
 }
