7a94cd5a4a070082f0c46797dfeea22cec9762aa
cicomsa <ioanacomsa85@gmail.com>
2020-05-13 01:27:12 +0100

    getData2 - 6 els

--- helpers/sixes.populate-data.js
+++ helpers/sixes.populate-data.js
@@ -24,37 +24,58 @@ const populateData1 = index => {
   return resultsObject
 }
 
-// six elements - triples + doubles
-// const populateData2 = (index, alternateVersion) => {
-//   const resultsObject = {}
-//   // triples top
-//   resultsObject[`${index}1`] = {}
-//   resultsObject[`${index}1`].initialState = populate(version8, alternateVersion, version2)
-//   resultsObject[`${index}2`] = {}
-//   resultsObject[`${index}2`].initialState = populate(version8, alternateVersion, version3)
-//   resultsObject[`${index}3`] = {}
-//   resultsObject[`${index}3`].initialState = populate(version8, alternateVersion, version4)
+// six elements - triples + doubles + singles
+const populateData2 = (index, alternateVersion) => {
+  const resultsObject = {}
+  // triples top
+  resultsObject[`${index}1`] = {}
+  resultsObject[`${index}1`].initialState = populate(version8, alternateVersion, version2)
+  resultsObject[`${index}2`] = {}
+  resultsObject[`${index}2`].initialState = populate(version8, alternateVersion, version3)
+  resultsObject[`${index}3`] = {}
+  resultsObject[`${index}3`].initialState = populate(version8, alternateVersion, version4)
 
-//   // triples middle
-//   resultsObject[`${index}4`] = {}
-//   resultsObject[`${index}4`].initialState = populate(alternateVersion, version8, version2)
-//   resultsObject[`${index}5`] = {}
-//   resultsObject[`${index}5`].initialState = populate(alternateVersion, version8, version3)
-//   resultsObject[`${index}6`] = {}
-//   resultsObject[`${index}6`].initialState = populate(alternateVersion, version8, version4)
+  resultsObject[`${index}4`] = {}
+  resultsObject[`${index}4`].initialState = populate(version8, version2, alternateVersion)
+  resultsObject[`${index}5`] = {}
+  resultsObject[`${index}5`].initialState = populate(version8, version3, alternateVersion)
+  resultsObject[`${index}6`] = {}
+  resultsObject[`${index}6`].initialState = populate(version8, version4, alternateVersion)
 
-//   // triples bottom
-//   resultsObject[`${index}7`] = {}
-//   resultsObject[`${index}7`].initialState = populate(alternateVersion, version2, version8)
-//   resultsObject[`${index}8`] = {}
-//   resultsObject[`${index}8`].initialState = populate(alternateVersion, version3, version8)
-//   resultsObject[`${index}9`] = {}
-//   resultsObject[`${index}9`].initialState = populate(alternateVersion, version4, version8)
+  // triples middle
+  resultsObject[`${index}7`] = {}
+  resultsObject[`${index}7`].initialState = populate(alternateVersion, version8, version2)
+  resultsObject[`${index}8`] = {}
+  resultsObject[`${index}8`].initialState = populate(alternateVersion, version8, version3)
+  resultsObject[`${index}9`] = {}
+  resultsObject[`${index}9`].initialState = populate(alternateVersion, version8, version4)
 
-//   return resultsObject
-// }
+  resultsObject[`${index}10`] = {}
+  resultsObject[`${index}10`].initialState = populate(version2, version8, alternateVersion)
+  resultsObject[`${index}11`] = {}
+  resultsObject[`${index}11`].initialState = populate(version3, version8, alternateVersion)
+  resultsObject[`${index}12`] = {}
+  resultsObject[`${index}12`].initialState = populate(version4, version8, alternateVersion)
 
-// // five elements - doubles + doubles + singles - part 1
+  // triples bottom
+  resultsObject[`${index}13`] = {}
+  resultsObject[`${index}13`].initialState = populate(alternateVersion, version2, version8)
+  resultsObject[`${index}14`] = {}
+  resultsObject[`${index}14`].initialState = populate(alternateVersion, version3, version8)
+  resultsObject[`${index}15`] = {}
+  resultsObject[`${index}15`].initialState = populate(alternateVersion, version4, version8)
+
+  resultsObject[`${index}16`] = {}
+  resultsObject[`${index}16`].initialState = populate(version2, alternateVersion, version8)
+  resultsObject[`${index}17`] = {}
+  resultsObject[`${index}17`].initialState = populate(version3, alternateVersion, version8)
+  resultsObject[`${index}18`] = {}
+  resultsObject[`${index}18`].initialState = populate(version4, alternateVersion, version8)
+
+  return resultsObject
+}
+
+// // six elements - doubles + doubles + singles - part 1
 // const populateData3 = (index, alternateVersion) => {
 //   const resultsObject = {}
 
@@ -82,66 +103,9 @@ const populateData1 = index => {
 //   return resultsObject
 // }
 
-// // five elements - doubles + doubles + singles - part 2
-// const populateData4 = (index, alternateVersion) => {
-//   const resultsObject = {}
-
-//   resultsObject[`${index}1`] = {}
-//   resultsObject[`${index}1`].initialState = populate(version2, version5, alternateVersion)
-//   resultsObject[`${index}2`] = {}
-//   resultsObject[`${index}2`].initialState = populate(version3, version5, alternateVersion)
-//   resultsObject[`${index}3`] = {}
-//   resultsObject[`${index}3`].initialState = populate(version4, version5, alternateVersion)
-
-//   resultsObject[`${index}4`] = {}
-//   resultsObject[`${index}4`].initialState = populate(version2, version6, alternateVersion)
-//   resultsObject[`${index}5`] = {}
-//   resultsObject[`${index}5`].initialState = populate(version3, version6, alternateVersion)
-//   resultsObject[`${index}6`] = {}
-//   resultsObject[`${index}6`].initialState = populate(version4, version6, alternateVersion)
-
-//   resultsObject[`${index}7`] = {}
-//   resultsObject[`${index}7`].initialState = populate(version2, version7, alternateVersion)
-//   resultsObject[`${index}8`] = {}
-//   resultsObject[`${index}8`].initialState = populate(version3, version7, alternateVersion)
-//   resultsObject[`${index}9`] = {}
-//   resultsObject[`${index}9`].initialState = populate(version4, version7, alternateVersion)
-
-//   return resultsObject
-// }
-
-// // five elements - doubles + doubles + singles - part 3
-// const populateData5 = (index, alternateVersion) => {
-//   const resultsObject = {}
-
-//   resultsObject[`${index}1`] = {}
-//   resultsObject[`${index}1`].initialState = populate(alternateVersion, version2, version5)
-//   resultsObject[`${index}2`] = {}
-//   resultsObject[`${index}2`].initialState = populate(alternateVersion, version3, version5)
-//   resultsObject[`${index}3`] = {}
-//   resultsObject[`${index}3`].initialState = populate(alternateVersion, version4, version5)
-
-//   resultsObject[`${index}4`] = {}
-//   resultsObject[`${index}4`].initialState = populate(alternateVersion, version2, version6)
-//   resultsObject[`${index}5`] = {}
-//   resultsObject[`${index}5`].initialState = populate(alternateVersion, version3, version6)
-//   resultsObject[`${index}6`] = {}
-//   resultsObject[`${index}6`].initialState = populate(alternateVersion, version4, version6)
-
-//   resultsObject[`${index}7`] = {}
-//   resultsObject[`${index}7`].initialState = populate(alternateVersion, version2, version7)
-//   resultsObject[`${index}8`] = {}
-//   resultsObject[`${index}8`].initialState = populate(alternateVersion, version3, version7)
-//   resultsObject[`${index}9`] = {}
-//   resultsObject[`${index}9`].initialState = populate(alternateVersion, version4, version7)
-
-//   return resultsObject
-// }
 
 module.exports = {
   populateData1,
-  // populateData2,
-  // populateData3,
-  // populateData4,
-  // populateData5
+  populateData2,
+  // populateData3
 }
