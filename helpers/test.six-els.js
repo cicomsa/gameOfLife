b6cf2d04b83256969ee9993f1798d63edb8a9e52
cicomsa <ioanacomsa85@gmail.com>
2020-05-13 01:30:00 +0100

    uncomment tests

--- helpers/test.six-els.js
+++ helpers/test.six-els.js
@@ -52,18 +52,33 @@ const getData2 = index => {
   return resultsObject
 }
 
-// const getData3 = index => {
-//   const resultsObject = populateData1(index, version7)
+const getData3 = index => {
+  const resultsObject = populateData2(index, version6)
 
-//   resultsObject[`${index}1`].result = populate(version7, version7, version1)
-//   resultsObject[`${index}2`].result = populate(version3, version1, version1)
-//   resultsObject[`${index}3`].result = populate(version7, version7, version1)
-//   resultsObject[`${index}4`].result = populate(version1, version7, version7)
-//   resultsObject[`${index}5`].result = populate(version1, version7, version7)
-//   resultsObject[`${index}6`].result = populate(version1, version7, version7)
+  resultsObject[`${index}1`].result = populate(version7, version1, version2)
+  resultsObject[`${index}2`].result = populate(version7, version1, version3)
+  resultsObject[`${index}3`].result = populate(version7, version2, version1)
+  resultsObject[`${index}4`].result = populate(version5, version1, version5)
+  resultsObject[`${index}5`].result = populate(version8, version1, version5)
+  resultsObject[`${index}6`].result = populate(version6, version4, version3)
 
-//   return resultsObject
-// }
+  resultsObject[`${index}7`].result = populate(version2, version4, version2)
+  resultsObject[`${index}8`].result = populate(version2, version4, version3)
+  resultsObject[`${index}9`].result = populate(version2, version7, version4)
+  resultsObject[`${index}10`].result = populate(version2, version4, version2)
+  resultsObject[`${index}11`].result = populate(version3, version4, version2)
+  resultsObject[`${index}12`].result = populate(version4, version7, version2)
+
+  resultsObject[`${index}13`].result = populate(version5, version1, version5)
+  resultsObject[`${index}14`].result = populate(version5, version1, version8)
+  resultsObject[`${index}15`].result = populate(version3, version4, version6)
+  resultsObject[`${index}16`].result = populate(version2, version1, version7)
+  resultsObject[`${index}17`].result = populate(version3, version1, version7)
+  resultsObject[`${index}18`].result = populate(version1, version2, version7)
+
+  return resultsObject
+}
+
 
 const getData4 = index => {
   const resultsObject = populateData2(index, version2)
