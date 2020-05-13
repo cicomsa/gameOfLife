f6407005d80b5cc2fd5678b9311298b82b3e70e6
cicomsa <ioanacomsa85@gmail.com>
2020-05-13 14:34:09 +0100

    getData2 - 7 up els

--- helpers/test.seven-up-els.js
+++ helpers/test.seven-up-els.js
@@ -35,15 +35,15 @@ const getData1 = index => {
 const getData2 = index => {
   const resultsObject = populateData2(index, version5)
 
-  resultsObject[`${index}1`].result = populate(version6, version1, version5)
-  resultsObject[`${index}2`].result = populate(version6, version2, version6)
-  resultsObject[`${index}3`].result = populate(version6, version2, version2)
-  resultsObject[`${index}4`].result = populate(version6, version1, version5)
-  resultsObject[`${index}5`].result = populate(version6, version1, version6)
-  resultsObject[`${index}6`].result = populate(version6, version1, version4)
-  resultsObject[`${index}7`].result = populate(version6, version7, version5)
-  resultsObject[`${index}8`].result = populate(version6, version2, version6)
-  resultsObject[`${index}9`].result = populate(version6, version7, version1)
+  resultsObject[`${index}1`].result = populate(version7, version1, version5)
+  resultsObject[`${index}2`].result = populate(version7, version1, version6)
+  resultsObject[`${index}3`].result = populate(version7, version1, version2)
+  resultsObject[`${index}4`].result = populate(version2, version4, version2)
+  resultsObject[`${index}5`].result = populate(version2, version1, version4)
+  resultsObject[`${index}6`].result = populate(version2, version4, version7)
+  resultsObject[`${index}7`].result = populate(version5, version1, version7)
+  resultsObject[`${index}8`].result = populate(version5, version1, version7)
+  resultsObject[`${index}9`].result = populate(version5, version4, version7)
 
   return resultsObject
 }
@@ -108,9 +108,9 @@ const testData = () => {
   resultsObject = {
     ...resultsObject,
     // triples + triples + singles
-    ...getData1('a'),
+    // ...getData1('a'),
     // triples + doubles + doubles
-    // ...getData2('b'),
+    ...getData2('b'),
     // ...getData3('c'),
     // ...getData4('d'),
     // // triples + triples + doubles
